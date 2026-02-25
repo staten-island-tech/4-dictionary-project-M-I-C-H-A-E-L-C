e8b33d010ac04c9b5ab2e93e21f02a3718313c33
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-02-25 10:31:22 -0500

    yyes

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,35 @@
 item = {
-    "name": "Bundle of 17 eggs",
+    "name": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps.",
     "price": 130.99000000000000000000013,
     "department": "Televisions",
     "mass": "1 solar mass, maybe like 55 pounds?",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "Bundle of 17 eggs"
 },
 {
-    "name": "costo hot chicken dog"
+    "name": "costo hot chicken dog",
     "price": 1.99,
-    "department":
-
+    "department": "Laundry Aisle",
+    "mass": "10 KG",
+    "description": "Chimken fud"
+},
+{
+    "name": "Cat fud",
+    "price": 3.99,
+    "department": "Laundry Aisle",
+    "mass": "10 Grams",
+    "description": "Very health for dogi"
+},
+{    
+    "name": "Empire state building ",
+    "price": 11.99,
+    "department": "Laundry Aisle",
+    "mass": "365,000,000,000.12389821398128490 Grams",
+    "description": "Big buildin"
+},
+{    
+    "name": "Linkedin ",
+    "price": "More than empire state building",
+    "department": "Construction tols",
+    "mass": "??? ",
+    "description": "Big moneh"
 },
